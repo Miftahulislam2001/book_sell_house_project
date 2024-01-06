ab4488e0a7adc81bf5f2f4bf0727a4d7cb51f460
miftahullislam <miftahullislamsohan@gmail.com>
2024-01-06 15:47:38 +0600

    card Showing Complate

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Cart from './Cart';
 
 const Books = () => {
+    const {books} = useLoaderData()
+  
     return (
-        <div>
-            <h3>This is Books Page</h3>
+        <div className='my-container'>
+            <div className='grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2'>
+            {
+                books.map(book => <Cart books = {book} key={book.isbn13}></Cart>)
+            }
+            </div>
         </div>
     );
 };
---
+++ src/Components/Cart.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Cart = ({ books }) => {
+    console.log(books);
+    // const { image, price, subtitle, title} = books;
+    return (
+        <div className='overflow-hidden relative transition duration-200 transform hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl'>
+            <img
+                src={books.image}
+                alt='book cover'
+                className='object-cover w-full h-56 md:h-64 xl:h-80'
+            />
+
+            <div className='bg-black px-6 py-4 bg-opacity-75 opacity-0 hover:opacity-100 text-gray-300 absolute inset-0 transition-opacity duration-200 flex flex-col'>
+                <p className='font-semibold text-xl'>{books.title}</p><br />
+                <p className='font-semibold text-md'>{books.subtitle.substring(0, 45)}...</p><br />
+                <p className='mt-auto font-semibold text-xl'>Price: {books.price}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
